e06ec7c3d1c1655ce98f811496d54e1e2562b798
wuliang6661 <wuliang6661@163.com>
2020-01-10 09:10:20 +0800

    修改包名

---
+++ app/src/main/java/com/skyvn/hw/api/HttpService.java
@@ -0,0 +1,20 @@
+package com.skyvn.hw.api;
+
+/**
+ * Created by wuliang on 2017/3/9.
+ * <p>
+ * 此处存放后台服务器的所有接口数据
+ */
+
+public interface HttpService {
+
+    //    String URL = "http://47.98.108.34:8080/";   //正式服
+    String URL = "http://47.96.126.117:8082/";   //测试服
+//    String URL = "http://mapi.platform.yinghezhong.com/";  //测试服2
+//    String URL = "http://api.open.yinghezhong.com/";  //正式环境
+//    String URL = "http://mapi.open.yinghezhong.com/";  //正式环境2
+
+
+  
+
+}
---
+++ app/src/main/java/com/skyvn/hw/view/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.skyvn.hw.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.skyvn.hw.base.BaseActivity;
+
+/**
+ * author : wuliang
+ * e-mail : wuliang6661@163.com
+ * date   : 2020/1/99:34
+ * desc   :
+ * version: 1.0
+ */
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
