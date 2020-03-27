edcb95d4a66b2132468b4d57629f85139f3d4d5a
wuliang6661 <wuliang6661@163.com>
2020-03-27 16:03:36 +0800

    首页 UI 和 订单页UI创建

--- app/src/main/java/com/skyvn/hw/api/HttpService.java
+++ app/src/main/java/com/skyvn/hw/api/HttpService.java
@@ -19,6 +19,7 @@ import com.skyvn.hw.bean.OrderDetailsBO;
 import com.skyvn.hw.bean.StatusBO;
 import com.skyvn.hw.bean.StsTokenBean;
 import com.skyvn.hw.bean.VersionBO;
+import com.skyvn.hw.config.IConstant;
 
 import java.util.List;
 import java.util.Map;
@@ -41,7 +42,7 @@ import rx.Observable;
 public interface HttpService {
 
     //    String URL = "http://tsa.fengyunv40.com/";   //正式服
-    String URL = "http://47.96.126.117:9989/";   //测试服
+    String URL = IConstant.URL;   //测试服
 //    String URL = "http://mapi.platform.yinghezhong.com/";  //测试服2
 //    String URL = "http://api.open.yinghezhong.com/";  //正式环境
 //    String URL = "http://mapi.open.yinghezhong.com/";  //正式环境2
--- app/src/main/java/com/skyvn/hw/config/IConstant.java
+++ app/src/main/java/com/skyvn/hw/config/IConstant.java
@@ -26,4 +26,26 @@ public interface IConstant {
      * 界面风格  1 :风格1  2：风格2
      */
     int STYLE = 1;
+
+    /**
+     * AppId
+     */
+    String appId = "1242415496784642049";
+
+    /**
+     * 服务器接口地址
+     */
+    String URL = "http://tsa.fengyunv40.com/";
+
+//    app打包：
+//    百倍云：
+//    saas-app：currentApplicationId：1237328537956216833
+//    接口地址：http://47.96.126.117:9989
+//    租户app：currentApplicationId：1237613520948129793 ，currentTenantId：1234763145872871425
+//    接口地址：http://47.96.126.117:9986
+//    客户：
+//    saas-app：currentApplicationId：1242415496784642049
+//    接口地址：http://tsa.fengyunv40.com
+//    租户app：currentApplicationId：1242353191250165761 ，currentTenantId：1242352620035608577
+//    接口地址：http://tta.fengyunv40.com
 }
