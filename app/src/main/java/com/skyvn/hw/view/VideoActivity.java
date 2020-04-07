e8f9059ea424c992a24637762876f00263b9a2f6
wuliang6661 <wuliang6661@163.com>
2020-04-07 13:27:59 +0800

    首页 UI 和 订单页UI创建

--- app/src/main/java/com/skyvn/hw/view/VideoActivity.java
+++ app/src/main/java/com/skyvn/hw/view/VideoActivity.java
@@ -189,7 +189,7 @@ public class VideoActivity extends BaseActivity implements ActionSheet.OnActionS
         intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
         intent.putExtra(MediaStore.EXTRA_SIZE_LIMIT, 1);
         intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
-        intent.putExtra("camerasensortype", 2); // 调用前置摄像头
+        intent.putExtra("android.intent.extras.VIDEO_CAMERA_FACING", 1);
         startActivityForResult(intent, 3);
 
     }
